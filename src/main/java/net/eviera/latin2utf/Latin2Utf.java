be09808f2f7938d9da0f667d7c3864f3334a5d13
emi@ideafix <eviera@ideafix>
2016-04-18 23:56:29 -0300

    Setup proyecto

--- src/main/java/net/eviera/latin2utf/Latin2Utf.java
+++ src/main/java/net/eviera/latin2utf/Latin2Utf.java
@@ -1,5 +1,12 @@
 package net.eviera.latin2utf;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Latin2Utf {
@@ -11,5 +18,26 @@ public class Latin2Utf {
 
     }
 
+
+    public void readFile(File file) {
+
+        try {
+            byte[] inBytes = Files.readAllBytes(Paths.get(file.toURI()));
+            byte[] outBytes = convertBytes(inBytes);
+
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "Error al leer el archivo [" + file +"]", e);
+        }
+    }
+
+    public byte[] convertBytes(byte[] inBytes) {
+        for (byte b : inBytes) {
+            if (b > 127) {
+                System.out.println(b);
+            }
+        }
+
+        return new byte[0];
+    }
 
 }
