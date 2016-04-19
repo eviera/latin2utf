96dfe612fc58a9601544a4c6b95634803d943187
emi@ideafix <eviera@ideafix>
2016-04-18 23:21:51 -0300

    Setup proyecto

---
+++ src/main/java/net/eviera/latin2utf/Latin2Utf.java
@@ -0,0 +1,15 @@
+package net.eviera.latin2utf;
+
+import java.util.logging.Logger;
+
+public class Latin2Utf {
+
+    private static final Logger log = Logger.getLogger(Latin2Utf.class.getName());
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
---
+++ src/test/java/net/eviera/latin2utf/Latin2UtfTest.java
@@ -0,0 +1,15 @@
+package net.eviera.latin2utf;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class Latin2UtfTest extends TestCase {
+
+    public void testApp() {
+        assertTrue(true);
+    }
+}
