5c15847e1ee86ae3d3d59d0b41bbbb9b3c275ef0
eviera@sdd <eviera@sdd>
2016-04-22 20:42:37 -0300

    conversion

--- src/main/java/net/eviera/latin2utf/Latin2Utf.java
+++ src/main/java/net/eviera/latin2utf/Latin2Utf.java
@@ -1,23 +1,38 @@
 package net.eviera.latin2utf;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Latin2Utf {
 
-    private static final Logger log = Logger.getLogger(Latin2Utf.class.getName());
-
     private Map<Byte, byte[]> conversionDict;
+    private String directory;
+    private String[] extensions;
+    private boolean convert;
 
-    public Latin2Utf() {
+
+    public Latin2Utf(String directory, String[] extensions, boolean convert) {
+
+        if (StringUtils.isEmpty(directory)) {
+            directory = ".";
+        }
+
+        this.extensions = extensions;
+        this.directory = directory;
+        this.convert = convert;
+
         populateConversionDictionary();
+
     }
 
     private void populateConversionDictionary() {
@@ -38,22 +53,48 @@ public class Latin2Utf {
         }
     }
 
-    public void converFile(File inFile) {
+    private void convertFiles() {
+        File directoryPath = new File(directory);
+        if (!directoryPath.isDirectory()) {
+            throw new RuntimeException("El parametro [" + directory + "] no es un directorio");
+        }
+
+        System.out.println("Procesando con directorio [" + directory + "], mascara " + Arrays.toString(extensions) + ", conversion [" + convert + "]");
+
+        Collection<File> files = FileUtils.listFiles(directoryPath, extensions, true);
+        for (File file : files) {
+            converFile(file, convert);
+        }
+
+    }
+
+    public void converFile(File file, boolean convert) {
 
         try {
-            byte[] inBytes = Files.readAllBytes(Paths.get(inFile.toURI()));
-            byte[] outBytes = convertBytes(inBytes);
-
+            byte[] inBytes = Files.readAllBytes(Paths.get(file.toURI()));
+            byte[] outBytes = analyze(file.getPath(), inBytes);
 
         } catch (IOException e) {
-            log.log(Level.SEVERE, "Error al leer el archivo [" + inFile + "]", e);
+            throw new RuntimeException("Error al leer el archivo [" + file + "]", e);
         }
     }
 
-    public byte[] convertBytes(byte[] inBytes) throws IOException {
+    public byte[] analyze(String filePath, byte[] inBytes) throws IOException {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        int line = 1;
+        int col = 0;
         for (byte b : inBytes) {
+            //Incrementa el contador de lineas al encontrar un LF
+            if ((b & 0xFF) == 0x0A) {
+                line++;
+                col = 0;
+            }
+
+            //Encontro un byte arriba de 127 (0x7F)
             if ((b & 0xFF) > 0x7F) {
+                System.out.printf("%s: [%03d][%03d] = ", filePath, line, col);
+
                 byte[] conversion = conversionDict.get(b);
                 if (conversion == null) {
                     throw new RuntimeException("No hay conversion para el byte [" + (b & 0xFF) + "]");
@@ -62,6 +103,9 @@ public class Latin2Utf {
             } else {
                 bos.write(b);
             }
+
+            //Incremento la columna
+            col++;
         }
         bos.close();
         return bos.toByteArray();
@@ -69,9 +113,63 @@ public class Latin2Utf {
 
 
     public static void main(String[] args) {
-        Latin2Utf latin2Utf = new Latin2Utf();
-        String borrar = Latin2Utf.class.getResource("/borrar.txt").getFile();
-        latin2Utf.converFile(new File(borrar));
+        try {
+            String folderArgument = null;
+            String[] extensions = null;
+            boolean convert = false;
+
+            //Parseo los argumentos
+            int argCount = 0;
+            for (String arg : args) {
+                if (arg.startsWith("--")) {
+                    //Estos argumentos no suman para el contador
+                    switch (arg) {
+                        case "--convert":
+                            convert = true;
+                            break;
+                        case "--help":
+                            printHelpAndExit(0);
+                            break;
+                        default:
+                            System.out.println("Argumento [" + arg + "] no reconocido");
+                            printHelpAndExit(1);
+                            break;
+                    }
+                }
+                switch (argCount++) {
+                    case 0:
+                        //Primer argumento: extensiones
+                        extensions = arg.split("[,]");
+                        break;
+                    case 1:
+                        //Segundo argumento: directorio
+                        folderArgument = arg;
+                        break;
+                    default:
+                        System.out.println("Argumento [" + arg + "] erroneo");
+                        printHelpAndExit(1);
+                }
+            }
+
+            Latin2Utf latin2Utf = new Latin2Utf(folderArgument, extensions, convert);
+            latin2Utf.convertFiles();
+
+        } catch (Exception e) {
+            System.err.println("Ha ocurrido un error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void printHelpAndExit(int status) {
+        System.out.println("Uso:");
+        System.out.println("Latin2Utf [opciones] extensiones [directorio]");
+        System.out.println("opciones: ");
+        System.out.println("   --help: imprime esta ayuda");
+        System.out.println("   --convert: efectua la conversion (si no se especifica, solo lista los cambios)");
+        System.out.println("extensiones: (requerido) extensiones de los distintos tipos de archivos a convertir separados por comas (ej: xml,java,jsp)");
+        System.out.println("directorio: si no se especifica usa el directorio actual");
+        System.exit(status);
     }
 
 }
--- src/test/java/net/eviera/latin2utf/Latin2UtfTest.java
+++ src/test/java/net/eviera/latin2utf/Latin2UtfTest.java
@@ -1,12 +1,9 @@
 package net.eviera.latin2utf;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import org.apache.commons.io.Charsets;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 
 /**
@@ -15,17 +12,15 @@ import java.util.Arrays;
 public class Latin2UtfTest extends TestCase {
 
     public void testConvertBytesSimple() throws IOException {
-        Latin2Utf l2u = new Latin2Utf();
+        Latin2Utf l2u = new Latin2Utf(null, null, false);
         String simple = "abc";
-        byte[] result = l2u.convertBytes(simple.getBytes(Charsets.ISO_8859_1));
+        byte[] result = l2u.analyze("", simple.getBytes(Charsets.ISO_8859_1));
         assertTrue("comparacion simple", Arrays.equals(new byte[]{(byte) 0x61, (byte) 0x62, (byte) 0x63}, result));
     }
 
     public void testConvertBytesAcute() throws IOException {
-        Latin2Utf l2u = new Latin2Utf();
-        StringBuilder dia = new StringBuilder();
-        dia.append("d").append((char) 237).append("a");
-        byte[] result = l2u.convertBytes(dia.toString().getBytes(Charsets.ISO_8859_1));
+        Latin2Utf l2u = new Latin2Utf(null, null, false);
+        byte[] result = l2u.analyze("", ("d" + (char) 237 + "a").getBytes(Charsets.ISO_8859_1));
         assertTrue("comparacion con acentos", Arrays.equals(new byte[]{(byte) 0x64, (byte) 0xC3, (byte) 0xAD, (byte) 0x61}, result));
     }
 
